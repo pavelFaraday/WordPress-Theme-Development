8f038a722acea98c9d93f58f84cd5376911f1e6f
“pavelFaraday” <devstudio.corp@gmail.com>
2025-09-29 12:11:18 +0400

    Add Stylesheet enqueueing action hook

--- app/public/wp-content/themes/aquila/functions.php
+++ app/public/wp-content/themes/aquila/functions.php
@@ -5,3 +5,11 @@
  *
  * @package Aquila
  */
+
+
+function aquila_enqueue_scripts()
+{
+    // wp_enqueue_style('stylesheet', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('stylesheet', get_stylesheet_uri());
+}
+add_action('wp_enqueue_scripts', 'aquila_enqueue_scripts');
--- app/public/wp-content/themes/aquila/index.php
+++ app/public/wp-content/themes/aquila/index.php
@@ -3,8 +3,6 @@
 /**
  * The main template file.
  *
- * 
- *
  * @package Aquila
  */
 ?>
