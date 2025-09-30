6b0551de0b7ded80701230a6477b5210b1829e7d
“pavelFaraday” <devstudio.corp@gmail.com>
2025-09-30 19:32:19 +0400

    - Register & Enqueue styles and scripts - add screenshot.png

--- app/public/wp-content/themes/aquila/functions.php
+++ app/public/wp-content/themes/aquila/functions.php
@@ -9,7 +9,13 @@
 
 function aquila_enqueue_scripts()
 {
+    // Register main stylesheet and JavaScript file
     // wp_enqueue_style('stylesheet', get_template_directory_uri() . '/style.css');
-    wp_enqueue_style('stylesheet', get_stylesheet_uri());
+    wp_register_style('style-css', get_stylesheet_uri());
+    wp_register_script('main-js', get_template_directory_uri() . '/assets/main.js', [], '1.0.0', true);
+
+    // Enqueue styles and scripts
+    wp_enqueue_style('style-css');
+    wp_enqueue_script('main-js');
 }
 add_action('wp_enqueue_scripts', 'aquila_enqueue_scripts');
